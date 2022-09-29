0bbc43fff45ee42483bad47f7137e78056367cd4
unknown <selmarionx@gmail.com>
2022-09-29 21:03:44 +0300

    Examples005_добавили ввод пользователя невзирая на регистр

---
+++ Examples005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "selmarionx")
+{
+    Console.WriteLine("Приветствуею тебя о ВЕЛИКИЙ ");
+}
+else 
+{
+    Console.Write("о привет смертный ");
+    Console.WriteLine(username);
+}
